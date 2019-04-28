d9b4febdb134652a276df3ad94e50b15606a5426
njsharrocks <23417048@edgehill.ac.uk>
2019-04-28 15:03:19 +0100

    final commit

--- Portfolio/dog/scripts.js
+++ Portfolio/dog/scripts.js
@@ -1,5 +1,6 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/*Simple javascript function to add and remove the responsive
+element of the navigation*/
+function navResponse() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
